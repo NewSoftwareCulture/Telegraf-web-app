96c2d1edc3c77df4dcd66fb4535cef5383648186
Vlados <vlados.pochta@gmail.com>
2022-05-16 02:56:51 +0400

    chore: release

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 echo "release started!"
 docker build -t telegram-web-app:1.0.0 .
-heroku container:push web --app telegram-web-app
-heroku container:release web --app telegram-web-app
+heroku container:push web --app telegram-web-app-staging
+heroku container:release web --app telegram-web-app-staging
 echo "release completed!"
